530e34a9f1246d4df7e95ba36fbf455aa4ea35a3
ch <mkmsacc@outlook.com>
2023-03-09 12:54:40 +0700

    Configure connection

---
+++ WebNettyClient/src/main/java/com/example/demo/DemoConnectionConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import io.netty.channel.ChannelOption;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.SerializationUtils;
+import reactor.netty.Connection;
+import reactor.netty.tcp.TcpClient;
+
+@Configuration
+public class DemoConnectionConfig {
+
+    private final DemoRepository repository;
+
+    public DemoConnectionConfig(DemoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Bean
+    public Connection connect() {
+        return TcpClient.create()
+                .host("localhost")
+                .port(9876)
+                .wiretap(true)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handle((nettyInbound, nettyOutbound) -> {
+                    nettyInbound
+                            .receive()
+                            .asByteArray()
+                            .cache(0)
+                            .subscribe(value -> repository.save((String) SerializationUtils.deserialize(value)));
+                    return nettyOutbound.neverComplete();
+                })
+                .connectNow();
+    }
+}
