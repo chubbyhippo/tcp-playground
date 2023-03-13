88d322df70b33cc086c71530af4934226939c702
ch <mkmsacc@outlook.com>
2023-03-13 16:55:19 +0700

    Change to byte send/receive

--- WebNettyClient/src/main/java/com/example/demo/DemoConnectionConfig.java
+++ WebNettyClient/src/main/java/com/example/demo/DemoConnectionConfig.java
@@ -29,7 +29,8 @@ public class DemoConnectionConfig {
                             .receive()
                             .asByteArray()
                             .cache(0)
-                            .subscribe(value -> repository.save((String) SerializationUtils.deserialize(value)));
+                            .subscribe(repository::save);
+
                     return nettyOutbound.neverComplete();
                 })
                 .connectNow();
