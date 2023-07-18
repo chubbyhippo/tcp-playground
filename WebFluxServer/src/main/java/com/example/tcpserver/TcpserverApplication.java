ea2c56b644c190c63ed79944589aa750fc9b6c0a
ch <mkmsacc@outlook.com>
2023-07-18 11:12:48 +0700

    Refactor code

--- WebFluxServer/src/main/java/com/example/tcpserver/TcpserverApplication.java
+++ WebFluxServer/src/main/java/com/example/tcpserver/TcpserverApplication.java
@@ -1,15 +1,9 @@
 package com.example.tcpserver;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelOption;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
-import reactor.netty.tcp.TcpClient;
 import reactor.netty.tcp.TcpServer;
-
-import java.util.HexFormat;
 
 @SpringBootApplication
 public class TcpserverApplication {
@@ -20,19 +14,12 @@ public class TcpserverApplication {
 
     @PostConstruct
     private void init() {
-        var res = HexFormat.of().parseHex("00363132333435363738393031323334353637383930313233343536373839304d31303030303130865add9abb9e9fac62f70b20fcbc405c");
         TcpServer.create()
-                .host("localhost")
                 .port(9876)
                 .wiretap(true)
-                .handle((nettyInbound, nettyOutbound) -> {
-
-                    return nettyOutbound.sendByteArray(
-                    nettyInbound.receive()
-                            .asByteArray()
-                            .map(bytes -> res));
-
-                })
+                .handle((nettyInbound, nettyOutbound) -> nettyOutbound.sendByteArray(
+                        nettyInbound.receive()
+                                .asByteArray()))
                 .bindNow()
                 .onDispose()
                 .block();
